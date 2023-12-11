2141cc0640250c520333640b452419cbce5f2a95
SimonS89 <simonrsueldo@gmail.com>
2023-12-11 18:15:52 -0300

    primer integrador

--- 20231211-SUELDO-ANGULAR/weather/script.js
+++ 20231211-SUELDO-ANGULAR/weather/script.js
@@ -7,20 +7,20 @@ const wicon = document.getElementById("wicon");
 const descripcion = document.getElementById("descripcion");
 
 btn.addEventListener("click", () => {
-  cargarCiudad(input.value);
+  let ciudadElegida = input.value;
+  cargarCiudad(ciudadElegida);
 });
 
 function cargarCiudad(value) {
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      value +
-      "&appid=fdd533266e28101881f610f2b8f1ebe1"
+    `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=fdd533266e28101881f610f2b8f1ebe1`
   )
     .then((response) => response.json())
     .then((data) => {
       ciudad.textContent = data.name;
       let aCelsius = (data.main.temp - 273.15).toFixed(1);
-      grados.textContent = aCelsius + "°C";
+      grados.textContent = `${aCelsius}°C`;
       descripcion.textContent = data.weather[0].description;
-    });
+    })
+    .catch((error) => alert(error));
 }
